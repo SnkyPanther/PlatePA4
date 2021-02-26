37138bed81ffd64cd0d08b9ff0ea9615d640d802
akochenkov <alexandr.kochenkov@gmail.com>
2021-02-26 00:03:10 -0800

    Final

--- main.h
+++ main.h
@@ -1,20 +1,30 @@
+/*  Function name : main.h
+*
+*   Purpose : has the name of the functions the struct node and the include(s)
+*
+*   Inputs
+*
+*   Return:
+*
+*   Side Effects : the good thing to have this is that I do not have to right the functions in all the programs
+*/
 #include <string.h>
-#include <stdio.h>
+#include <stdio.h>		//the include(s)
 #include <stdlib.h>
 struct node {
-  char *plate;
+  char *plate;			//The struct node structure
   char *first;
   char *last;
   struct node *left;
   struct node *right;
 };
 
-typedef struct node* Node;
+typedef struct node* Node;		//Node definition
 
 int height(Node root);
 int balanced(Node root);
 Node add(Node root,
-	char *plate,
+	char *plate,			//function names
 	char *first,
 	char *last);
 int search(Node root,
