d36cccc43e6d929b56cc6846aa68a7860e2af5da
akochenkov <alexandr.kochenkov@gmail.com>
2021-02-25 23:02:18 -0800

    Final comments

--- balanced.c
+++ balanced.c
@@ -14,17 +14,17 @@
 
 
 int balanced(Node root) {
-  if(root==NULL)  {
+  if(root==NULL)  {				//if the root is NULL the functions return 1
     return 1;
   }
-  if(height(root->left)-height(root->right) <= 1) {
-    if(balanced(root->left) == 1) {
-      if(balanced(root->right) == 1) {
+  if(height(root->left)-height(root->right) <= 1) { 	//checks if the differences of the heights right and left trees are less 
+    if(balanced(root->left) == 1) {			// that equal to 1, and then checks recursively if the root->left and root->right
+      if(balanced(root->right) == 1) {			//are both balanced then it returns 1, which means that it is balanced
         return 1;
       }
     }
     else {
-      return 0;
+      return 0;						//if not then it returns 0
     }
   }  
   return 0;  
